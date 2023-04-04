35c365190fc65e703b99bdbfa457b0f81d0d3ec5
madebyfabian <hello@madebyfabian.com>
2023-04-04 16:09:06 +0200

    fix: remove unnecessary check

--- server/api/imap/user.ts
+++ server/api/imap/user.ts
@@ -2,11 +2,6 @@
  * This endpoint is used to retrieve the stored user data from the cookie.
  */
 export default defineEventHandler(async event => {
-	const passwordSalt = useRuntimeConfig()?.imapPasswordSalt
-	if (!passwordSalt) {
-		throw createError({ statusCode: 500, message: 'No password salt set' })
-	}
-
 	const user = getUserFromCookie({ event })
 	const userConfig = getSafeUserConfigFromCookie({ event })
 	const davUser = getSafeDavUserFromCookie({ event })
